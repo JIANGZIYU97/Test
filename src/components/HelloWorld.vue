a768d70d43482f10e9c44037fe3edeb7ebd22451
JiangZiYu <jiangziyu0204@163.com>
2023-07-26 22:46:53 +0800

    cxy1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -24,9 +24,7 @@
                             <el-dropdown-item>删除</el-dropdown-item>
                         </el-dropdown-menu>
                     </el-dropdown>
-                    <span>王小龙devJzy</span>
-                    <span>王小龙devJzy</span>
-                    <span>王小龙devJzy</span>
+                    <span>王小龙devCxy</span>
                 </el-header>
 
                 <el-main>
